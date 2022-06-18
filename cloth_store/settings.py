429cb9b970706ab9f5d994ad8b3e313172a8575e
gautamk.inexture <gautamk.inexture@gmail.com>
2022-06-18 13:37:59 +0530

    updated

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_VAL')
+SECRET_KEY = 'django-insecure-((g*j(krq&8n%ob)#2g#xhe2j!*l3hj18fcqlvldlqd+7_t&#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
