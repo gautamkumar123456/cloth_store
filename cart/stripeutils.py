14b1c719e69397c76f1401f863ef67905872b537
gautamk.inexture <gautamk.inexture@gmail.com>
2022-07-12 14:45:09 +0530

    domain and host fix

--- cart/stripeutils.py
+++ cart/stripeutils.py
@@ -7,7 +7,7 @@ def stripe_session_create(data):
         payment_method_types=['card'],
         line_items=[data],
         mode='payment',
-        success_url=f"{settings.HOST}api/cart/success",
-        cancel_url=f"{settings.HOST}api/cart/cancel",
+        success_url=f"{settings.DOMAIN}api/cart/success",
+        cancel_url=f"{settings.DOMAIN}api/cart/cancel",
     )
     return session
