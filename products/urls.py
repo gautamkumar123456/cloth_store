331953d3a4923c83142b9d82b6eb16aa650dc7eb
gautamk.inexture <gautamk.inexture@gmail.com>
2022-07-13 15:25:01 +0530

    urls

--- products/urls.py
+++ products/urls.py
@@ -10,14 +10,14 @@ urlpatterns = [
     path('category_view/<int:pk>', CategoryView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='category'),
     path('size_view/<int:pk>', SizeView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='size'),
     path('size_view/', SizeView.as_view({'get': 'list', 'post': 'create'}), name='size'),
-    path('color_view/<int:pk>', SizeView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='color'),
-    path('color_view/', SizeView.as_view({'get': 'list', 'post': 'create'}), name='color'),
-    path('brand_view/<int:pk>', SizeView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='brand'),
-    path('brand_view/', SizeView.as_view({'get': 'list', 'post': 'create'}), name='brand'),
-    path('season_view/<int:pk>', SizeView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='season'),
-    path('season_view/', SizeView.as_view({'get': 'list', 'post': 'create'}), name='season'),
-    path('quality_view/<int:pk>', SizeView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='quality'),
-    path('quality_view/', SizeView.as_view({'get': 'list', 'post': 'create'}), name='quality')
+    path('color_view/<int:pk>', ColorView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='color'),
+    path('color_view/', ColorView.as_view({'get': 'list', 'post': 'create'}), name='color'),
+    path('brand_view/<int:pk>', BrandView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='brand'),
+    path('brand_view/', BrandView.as_view({'get': 'list', 'post': 'create'}), name='brand'),
+    path('season_view/<int:pk>', SeasonView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='season'),
+    path('season_view/', SeasonView.as_view({'get': 'list', 'post': 'create'}), name='season'),
+    path('quality_view/<int:pk>', QualityView.as_view({'put': 'partial_update', 'post': 'destroy'}), name='quality'),
+    path('quality_view/', QualityView.as_view({'get': 'list', 'post': 'create'}), name='quality')
 
 
 
